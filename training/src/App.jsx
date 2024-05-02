0995bb214821ca87c10cad01f1619679e5407a45
Egor Gavrilov <egoregorgavrilov@gmail.com>
2024-05-02 22:10:11 +0200

    first commit

---
+++ training/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function WhoAmI ({name, surname, link}) {
+	return (
+		<div>
+			<h1>My name is {name.firstName}, surname - {surname}</h1>
+			<a href={link}>My profile</a>
+		</div>
+	)
+}
+
+function App() {
+	return (
+		<div className="App">
+			<WhoAmI name={() => {return 'John'}} surname='Smith' link='facebook.com'/>
+			<WhoAmI name={() => {return 'Alex'}} surname='Shepard' link='vk.com'/>
+		</div>
+	)
+}
+
+export default App;
